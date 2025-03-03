8bdc8db19b45a871beec16f571c4d6ec1cf7e342
Erkebulan100 <67504870+Erkebulan100@users.noreply.github.com>
2025-03-03 12:39:12 +0600

    Initial commit with AWS Amplify + Cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "drealestaterentalassa6f975ac": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
